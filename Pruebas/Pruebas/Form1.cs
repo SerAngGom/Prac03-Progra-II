f02209ee54f66897257e3017dc156b8b7602dc4e
SerAngGom <al405280@uji.es>
2023-04-25 19:15:33 +0200

    escaso avance

--- Pruebas/Pruebas/Form1.cs
+++ Pruebas/Pruebas/Form1.cs
@@ -16,11 +16,17 @@ namespace Pruebas
         public static int puntos;
         public static int tiempo;
         private Marcador marcador;
+        private Serpiente serpiente;
+        private Segmento cabeza;
+        private Segmento cola;
         public Form1()
         {
             InitializeComponent();
             marcador = new Marcador();
             Controls.Add(marcador.MiLabel);
+            //cabeza = new Segmento(Color.Green,200,200);
+            //Controls.Add(cabeza.MiPictureBox);
+            serpiente = new Serpiente(Controls);  
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -38,6 +44,35 @@ namespace Pruebas
                 MiLabel.Top = 20;
                 MiLabel.Text = "Puntos: " + Convert.ToString(puntos) + "\n" + "Tiempo de vida: " + Convert.ToString(tiempo);
             }
+        }
+        public class Serpiente
+        {
+            private Segmento cabeza;
+            private Segmento cola;
+            private Control.ControlCollection controls;
+
+            public Serpiente(Control.ControlCollection c)
+            {
+                cabeza = new Segmento(Color.Green, 200, 200);
+                c.Add(cabeza.MiPictureBox);
+                
+            }
+
+        }
+
+        public class Segmento
+        {
+            public PictureBox MiPictureBox = new PictureBox();
+    
+            public Segmento(Color color, int posicionx, int posiciony)
+            {
+                MiPictureBox.BackColor = color;
+                MiPictureBox.Top= posicionx;
+                MiPictureBox.Left = posiciony;
+                MiPictureBox.Width = 25;
+                MiPictureBox.Height = 25;
+            }
+
         }
     }
 }
