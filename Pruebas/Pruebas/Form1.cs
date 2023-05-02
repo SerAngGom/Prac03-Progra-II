75809b95ac390d0744d94670c26e08bfac448d18
SerAngGom <al405280@uji.es>
2023-05-02 19:17:22 +0200

    avanzando en el 10

--- Pruebas/Pruebas/Form1.cs
+++ Pruebas/Pruebas/Form1.cs
@@ -19,11 +19,15 @@ namespace Pruebas
         private Serpiente serpiente;
         private Segmento cabeza;
         private Segmento cola;
+        public const Keys arriba = Keys.Up;
+        public const Keys abajo = Keys.Down;
+        public const Keys izquierda = Keys.Left;
+        public const Keys derecha = Keys.Right;
+        public Keys direccion = Keys.Right;
         public Form1()
         {
             InitializeComponent();
             marcador = new Marcador();
-            Controls.Add(marcador.MiLabel);
             //cabeza = new Segmento(Color.Green,200,200);
             //Controls.Add(cabeza.MiPictureBox);
             serpiente = new Serpiente(Controls);  
@@ -47,32 +51,112 @@ namespace Pruebas
         }
         public class Serpiente
         {
+            private int posx {get;set;}
+            private int posy { get; set; }
+            private int posxanterior { get; set; }
+            private int posyanterior { get; set; }
+            private int separacion = 25;
+            private int n_cola = 1;
             private Segmento cabeza;
-            private Segmento cola;
             private Control.ControlCollection controls;
+            public List<Segmento> cola = new List<Segmento>();
 
             public Serpiente(Control.ControlCollection c)
             {
-                cabeza = new Segmento(Color.Green, 200, 200);
+                posx = 200;
+                posy = 200;
+                cabeza = new Segmento(Color.Green, posx, posy);
+                cola.Add(cabeza);
+                cola.Add(new Segmento(Color.Blue, posx - separacion * n_cola, posy));
+                n_cola++;
+                cola.Add(new Segmento(Color.Blue, posx - separacion * n_cola, posy));
+                n_cola++;
+                foreach (Segmento segmento in cola)
+                {
+                    c.Add(segmento.MiPictureBox);
+                }
                 c.Add(cabeza.MiPictureBox);
-                
+
             }
+            public void MoverSerpiente (Keys direccion)
+            {
+                if (direccion == arriba)
+                {
+                    cola[0].CambiaPosY(-5);
+                }
+                else if (direccion == abajo)
+                {
+                    cola[0].CambiaPosY(5);
+                }
+                else if (direccion == izquierda)
+                {
+                    cola[0].CambiaPosX(-5);
+                }
+                else if (direccion == derecha)
+                {
+                    cola[0].CambiaPosX(5);
+                }
+            }
+
 
         }
 
-        public class Segmento
+        public class Segmento 
         {
             public PictureBox MiPictureBox = new PictureBox();
-    
+
             public Segmento(Color color, int posicionx, int posiciony)
             {
                 MiPictureBox.BackColor = color;
-                MiPictureBox.Top= posicionx;
-                MiPictureBox.Left = posiciony;
+                MiPictureBox.Top= posiciony;
+                MiPictureBox.Left = posicionx;
                 MiPictureBox.Width = 25;
                 MiPictureBox.Height = 25;
             }
+            public void CambiaPosX(int n)
+            {
+                MiPictureBox.Left = MiPictureBox.Left + n;
+            }
+            public void CambiaPosY(int n) 
+            { 
+                MiPictureBox.Top = MiPictureBox.Top + n;
+            }
 
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e, Keys direccion)
+        {
+            switch (e.KeyCode)
+            {
+                case arriba:
+                    direccion = arriba;
+                    break;
+                case abajo:
+                    direccion = abajo;
+                    break;
+                case izquierda:
+                    direccion = izquierda;
+                    break;
+                case derecha:
+                    direccion = derecha;
+                    break;
+                default:
+                    this.direccion = direccion;
+                    break;
+
+            }
+
+            serpiente.MoverSerpiente(direccion);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e, Control.ControlCollection c, List<Segmento>  cola)
+        {
+            foreach (Segmento segmento in cola)
+            {
+                c.Add(segmento.MiPictureBox);
+            }
+            c.Add(cabeza.MiPictureBox);
+            Controls.Add(marcador.MiLabel);
+            this.Invalidate();
         }
     }
 }
